e2f927c2609e879cb0f4c76fbeb7e05f49fd75bd
Ahkar Shwebaw <arkarshwebaw@gmail.com>
2020-11-26 22:42:22 +0630

    Add MVC to project

--- AKSB.BookStore/Startup.cs
+++ AKSB.BookStore/Startup.cs
@@ -16,6 +16,7 @@ namespace AKSB.BookStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +27,11 @@ namespace AKSB.BookStore
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
+            app.UseRouting();//map url to particular resource
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
