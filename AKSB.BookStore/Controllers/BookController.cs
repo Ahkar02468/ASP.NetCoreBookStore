97b4e2c83bd8b7b9c1be168c47e2f4ae020836c0
Ahkar Shwebaw <arkarshwebaw@gmail.com>
2020-11-26 23:45:11 +0630

    Test with controller
    Add prototype controller to BookController

---
+++ AKSB.BookStore/Controllers/BookController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AKSB.BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        public string GetAllBooks()
+        {
+            return "All books.";
+        }
+
+        public string GetBook(int id)
+        {
+            return $"Book with id of {id}.";
+        }
+
+        public string SearchBooks(string bookName,string authorName)
+        {
+            return $"Book with name {bookName} and author is {authorName}";
+        }
+    }
+}
