275c0322d98635236c120dc8776066411c6741bc
Ahkar Shwebaw <arkarshwebaw@gmail.com>
2020-12-01 14:49:53 +0630

    Configure EF Core

--- AKSB.BookStore/Startup.cs
+++ AKSB.BookStore/Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AKSB.BookStore.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -16,6 +18,8 @@ namespace AKSB.BookStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<BookStoreContext>
+                (options => options.UseSqlServer("Server=.;Database=BookStore;Integrated Security=true"));
             services.AddControllersWithViews();
 #if DEBUG
             services.AddRazorPages().AddRazorRuntimeCompilation();
